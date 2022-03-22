fec87d380dd0de0a70f39929e48a13c8a87e7697
Taro Sakazume <ts95.business@gmail.com>
2022-03-22 10:00:02 +0900

    implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+
   # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
     if session[:user_id]
@@ -12,6 +20,12 @@ module SessionsHelper
         @current_user = User.find_by(id: session[:user_id])
       else
         @current_user
+      end
+    elsif cookies.signed[:user_id] # 記憶トークンcookieに対応するユーザーを返す
+      user = User.find_by(id: cookies.signed[:user_id])
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
       end
     end
   end
@@ -23,6 +37,20 @@ module SessionsHelper
 
   # 現在のユーザーをログアウトする
   def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,3 +17,13 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+end
